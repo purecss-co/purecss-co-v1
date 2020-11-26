5168f92a2cf2d0356cd26756c6941b7b6bc5b847
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-11-27 02:30:00 +0800

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
+require("mix-html-builder");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,23 +12,49 @@ let mix = require('laravel-mix');
  |
  */
 
- mix.js('src/js/app.js', 'js')
-     .sass('src/sass/app.scss', 'css')
-     .options({
-          postCss: [ 
-              require('autoprefixer'),
-              require('cssnano'),
-          ],
-          processCssUrls: true
-     })
-     .browserSync({
-        proxy: '127.0.0.1:8000',
-        files: [
-          '*.html',
-          'css/*.css',
-          'js/*.js',
-        ],
+mix.setPublicPath("dist")
+    .html({
+        htmlRoot: "src/views/**/*.html",
+        output: "../dist",
+        partialRoot: "src/views/partials",
+        layoutRoot: "src/views/layouts",
+        inject: true,
+        versioning: true,
+        minify: {
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            conservativeCollapse: true,
+        },
+    })
+    .js("src/js/app.js", "dist/js")
+    .sass("src/sass/app.scss", "dist/css")
+    .options({
+        postCss: [require("autoprefixer"), require("cssnano")],
+        processCssUrls: true,
+    })
+    .browserSync({
+        proxy: "127.0.0.1:8000",
+        files: ["dist/**/*.html", "dist/css/*.css", "dist/js/*.js"],
         open: true,
         notify: false,
-     });    
+    })
+    .version();
 
+mix.extend("addWebpackLoaders", (webpackConfig, loaderRules) => {
+    loaderRules.forEach(loaderRule => {
+        webpackConfig.module.rules.push(loaderRule);
+    });
+});
+
+mix.addWebpackLoaders([
+    {
+        test: /\.scss$/,
+        loader: "import-glob-loader",
+    },
+]);
